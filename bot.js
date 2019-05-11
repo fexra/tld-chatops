a8ead2765779afabc384e7848cb1d9dc7a708297
fexra <fexra@protonmail.com>
2019-05-11 03:40:50 +0200

    http server

--- bot.js
+++ bot.js
@@ -2,35 +2,50 @@
 //
 // Please see the included LICENSE file for more information.
 
-"use strict";
+`use strict`;
 
-const Config = require("./config.json");
-const Discord = require("discord.js");
-const DNS = require("dns");
-const util = require("util");
-const Address4 = require("ip-address").Address4;
-const Address6 = require("ip-address").Address6;
-const Sqlite3 = require("sqlite3");
-const Octokit = require("@octokit/rest");
+const Config = require(`./config.json`);
+const http = require(`http`);
+const DNS = require(`dns`);
+const util = require(`util`);
+const Address4 = require(`ip-address`).Address4;
+const Address6 = require(`ip-address`).Address6;
+const Sqlite3 = require(`sqlite3`);
+const Discord = require(`discord.js`);
+const Octokit = require(`@octokit/rest`);
+
+// Setup HTTP server for webhooks and poop
+
+http
+  .createServer(function(req, res) {
+    var addr = this.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    log("Listening on " + bind);
+
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.write(req.url);
+    res.end();
+  })
+  .listen(Config.port || 8020);
 
 // Connect to DB
 const db = new Sqlite3.Database(Config.database, err => {
   if (err) {
-    log("Could not connect to backend database");
+    log(`Could not connect to backend database`);
     process.exit(1);
   }
   run(
     [
-      "CREATE TABLE IF NOT EXISTS ",
-      "banned_users ",
-      "(id TEXT PRIMARY KEY, nickname TEXT, domain TEXT, timestamp DATETIME)"
-    ].join("")
+      `CREATE TABLE IF NOT EXISTS `,
+      `banned_users `,
+      `(id TEXT PRIMARY KEY, nickname TEXT, domain TEXT, timestamp DATETIME)`
+    ].join(``)
   )
     .then(() => {
-      log("Connected to backend database");
+      log(`Connected to backend database`);
     })
     .catch(() => {
-      log('Could not create "banned_users" table');
+      log("Could not create `banned_users` table");
       process.exit(1);
     });
 });
@@ -39,7 +54,7 @@ const db = new Sqlite3.Database(Config.database, err => {
 const octokit = Octokit({
   auth: Config.githubAuth,
   userAgent: Config.githubAgent,
-  baseUrl: "https://api.github.com",
+  baseUrl: `https://api.github.com`,
   log: {
     debug: () => {},
     info: () => {},
@@ -64,7 +79,7 @@ function run(query, args) {
 }
 
 function log(message) {
-  console.log(util.format("%s: %s", new Date().toUTCString(), message));
+  console.log(util.format(`%s: %s`, new Date().toUTCString(), message));
 }
 
 function isGod(id) {
@@ -81,7 +96,7 @@ const client = new Discord.Client();
 
 if (!process.env.DISCORDTOKEN) {
   throw new Error(
-    "You must supply the BOT Discord Token in the enviromental variable DISCORDTOKEN"
+    `You must supply the BOT Discord Token in the enviromental variable DISCORDTOKEN`
   );
 }
 
@@ -114,34 +129,34 @@ function tryMessageReact(message, reaction) {
 
 function resolveHostname(hostname) {
   return new Promise((resolve, reject) => {
-    DNS.resolve(hostname, "A", (err, records) => {
+    DNS.resolve(hostname, `A`, (err, records) => {
       if (err) return reject(err);
       return resolve(records);
     });
   });
 }
 
-client.on("ready", () => {
+client.on(`ready`, () => {
   log(`Logged in as ${client.user.tag}!`);
 
-  client.user.setActivity("with DNS");
+  client.user.setActivity(`with DNS`);
 
   const statusChannel = client.channels.get(Config.channels.status);
 
   tryChannelSendMessage(
     statusChannel,
-    ":bee: :honey_pot: :fire: :wind_blowing_face: :drooling_face:"
+    `:bee: :honey_pot: :fire: :wind_blowing_face: :drooling_face:`
   ).then(success => {
     if (success) {
-      log("Servers:");
-      client.guilds.forEach(guild => log(" - " + guild.name));
+      log(`Servers:`);
+      client.guilds.forEach(guild => log(` - ` + guild.name));
     } else {
-      log("Could not send a message to the configured status channel");
+      log(`Could not send a message to the configured status channel`);
     }
   });
 });
 
-client.on("message", receivedMessage => {
+client.on(`message`, receivedMessage => {
   // const statusChannel = client.channels.get(Config.channels.status)
 
   if (receivedMessage.author === client.user) {
@@ -152,10 +167,10 @@ client.on("message", receivedMessage => {
     commandHandler(receivedMessage);
   }
 
-  if (receivedMessage.content === `${Config.trigger}trtl`) {
+  if (receivedMessage.content === `${Config.trigger}ping`) {
     const currentdate = Date();
-    tryMessageReact(receivedMessage, "🐢");
-    log(currentdate, "\n PONG");
+    tryMessageReact(receivedMessage, `🐢`);
+    log(currentdate, `\n PONG`);
   }
 });
 
@@ -164,42 +179,42 @@ client.login(process.env.DISCORDTOKEN);
 
 function commandHandler(receivedMessage) {
   let fullCommand = receivedMessage.content.substr(6);
-  let splitCommand = fullCommand.split(" ");
+  let splitCommand = fullCommand.split(` `);
   let primaryCommand = splitCommand[0];
   let args = splitCommand.slice(1);
 
-  log("\nCommand received: " + primaryCommand);
-  log("TYPE: " + args[0]);
-  log("DEST: " + args[1]);
-  log("SOURCE: " + args[2]);
+  log(`\nCommand received: ` + primaryCommand);
+  log(`TYPE: ` + args[0]);
+  log(`DEST: ` + args[1]);
+  log(`SOURCE: ` + args[2]);
 
   const statusChannel = client.channels.get(Config.channels.status);
 
   tryChannelSendMessage(
     statusChannel,
     util.format(
-      "Message received from %s: %s",
+      `Message received from %s: %s`,
       receivedMessage.author.toString(),
       receivedMessage.content
     )
   );
 
   switch (primaryCommand.toLowerCase()) {
-    case "help":
+    case `help`:
       return helpCommand(receivedMessage);
-    case "register":
+    case `register`:
       return registerCommand(args, receivedMessage);
-    case "check":
+    case `check`:
       return checkDomainARecord(args, receivedMessage);
-    case "accept":
+    case `accept`:
       return applyDNSRecord(args, receivedMessage);
-    case "reject":
+    case `reject`:
       return rejectDNSRecord(args, receivedMessage);
 
     default:
       return tryChannelSendMessage(
         receivedMessage.channel,
-        "That is not a command I understand yet"
+        `That is not a command I understand yet`
       );
   }
 }
@@ -207,7 +222,7 @@ function commandHandler(receivedMessage) {
 function helpCommand(receivedMessage) {
   tryChannelSendMessage(
     receivedMessage.channel,
-    "HELP!\nActivate me with **.trtl <command>**\nCommands: `register`, `help`, `check`"
+    `HELP!\nActivate me with **.trtl <command>**\nCommands: "register", "help", "check"`
   );
 }
 
@@ -215,43 +230,43 @@ function registerCommand(args, receivedMessage) {
   if (args.length < 3) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "Not enough values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`"
+      `Not enough values to register. The syntax should be ".trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>"`
     );
-    return tryMessageReact(receivedMessage, "💩");
+    return tryMessageReact(receivedMessage, `💩`);
   }
   if (args.length > 3) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "Too many values to register. The syntax should be `.trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>`"
+      `Too many values to register. The syntax should be ".trtl register <TYPE> <DESTINATION> <SOURCE/VALUE>"`
     );
-    return tryMessageReact(receivedMessage, "💩");
+    return tryMessageReact(receivedMessage, `💩`);
   }
   if (args.length === 3) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "Number of arguments is correct"
+      `Number of arguments is correct`
     );
   }
 
-  const recordTypes = ["A", "TXT", "CNAME"]; // Record types we handle
+  const recordTypes = [`A`, `TXT`, `CNAME`]; // Record types we handle
 
   /* Verify that our first argument is indeed one of the record types that
      we know how to handle or exit early */
   if (recordTypes.indexOf(args[0].toUpperCase()) === -1) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "This is not a record type I understand yet. Options: `A`, `TXT`, `CNAME`."
+      `This is not a record type I understand yet. Options: "A", "TXT", "CNAME".`
     );
-    return tryMessageReact(receivedMessage, "💩");
+    return tryMessageReact(receivedMessage, `💩`);
   }
 
   const freeTierCommunities = [
-    ".fork.trtl",
-    ".pool.trtl",
-    ".user.trtl",
-    ".node.trtl",
-    ".dev.trtl",
-    ".bot.trtl"
+    `.fork.trtl`,
+    `.pool.trtl`,
+    `.user.trtl`,
+    `.node.trtl`,
+    `.dev.trtl`,
+    `.bot.trtl`
   ]; // Community suffixes we handle
 
   const validDomain = freeTierCommunities.some(domain =>
@@ -262,38 +277,39 @@ function registerCommand(args, receivedMessage) {
   if (!validDomain) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "This is not a community suffix I recognize yet. Options: `.fork.trtl`, `.pool.trtl`, `.user.trtl`, `.node.trtl`, `.dev.trtl`, `.bot.trtl`"
+      `This is not a community suffix I recognize yet. Options: `.fork.trtl`, `
+        .pool.trtl`, `.user.trtl`, `.node.trtl`, `.dev.trtl`, `.bot.trtl``
     );
-    return tryMessageReact(receivedMessage, "🐢");
+    return tryMessageReact(receivedMessage, `🐢`);
   } else {
-    tryChannelSendMessage(receivedMessage.channel, "**[PASS]** Suffix type ");
-    return tryMessageReact(receivedMessage, "💩");
+    tryChannelSendMessage(receivedMessage.channel, `**[PASS]** Suffix type `);
+    return tryMessageReact(receivedMessage, `💩`);
   }
 
   // legacy community validation handler
   // if (freeTierCommunities.includes(args[1].toUpperCase())) {
   //     tryChannelSendMessage(
   //         receivedMessage.channel,
-  //         "This is not a community suffix I recognize yet. Options: `.fork.trtl`, `.pool.trtl`, `.user.trtl`, `.node.trtl`, `.dev.trtl`, `.bot.trtl`"
+  //         `This is not a community suffix I recognize yet. Options: `.fork.trtl`, `.pool.trtl`, `.user.trtl`, `.node.trtl`, `.dev.trtl`, `.bot.trtl``
   //     );
-  //     return tryMessageReact(receivedMessage, "💩");
+  //     return tryMessageReact(receivedMessage, `💩`);
   // }
   // if (args[1].endsWith(freeTierCommunities)) {
   //     tryChannelSendMessage(
   //         receivedMessage.channel,
-  //         "[PASS] Community type "
+  //         `[PASS] Community type `
   //     );
-  //     return tryMessageReact(receivedMessage, "🐢");
+  //     return tryMessageReact(receivedMessage, `🐢`);
   // }
 }
 
 function checkDomainARecord(args, receivedMessage) {
-  if (args.length !== 1 || args[0].toLowerCase().indexOf(".trtl") === -1) {
+  if (args.length !== 1 || args[0].toLowerCase().indexOf(`.trtl`) === -1) {
     tryChannelSendMessage(
       receivedMessage.channel,
-      "You must specify a .trtl domain to check. The syntax should be `.trtl check <DOMAIN>.trtl`"
+      `You must specify a .trtl domain to check. The syntax should be ".trtl check <DOMAIN>.trtl"`
     );
-    return tryMessageReact(receivedMessage, "💩");
+    return tryMessageReact(receivedMessage, `💩`);
   }
 
   resolveHostname(args[0].toLowerCase())
@@ -301,19 +317,19 @@ function checkDomainARecord(args, receivedMessage) {
       tryChannelSendMessage(
         receivedMessage.channel,
         util.format(
-          "Resolved %s to %s",
+          `Resolved %s to %s`,
           args[0].toLowerCase(),
-          records.join(",")
+          records.join(`,`)
         )
       );
-      return tryMessageReact(receivedMessage, "🐢");
+      return tryMessageReact(receivedMessage, `🐢`);
     })
     .catch(() => {
       tryChannelSendMessage(
         receivedMessage.channel,
-        "Could not resolve the specified domain"
+        `Could not resolve the specified domain`
       );
-      return tryMessageReact(receivedMessage, "💩");
+      return tryMessageReact(receivedMessage, `💩`);
     });
 }
 
@@ -323,8 +339,8 @@ function applyDNSRecord(args, receivedMessage) {
   if (isGod(message.author.id)) {
     // Do logic here
   } else {
-    tryChannelSendMessage(receivedMessage.channel, "naughty turtle");
-    return tryMessageReact(receivedMessage, ":brainlet:");
+    tryChannelSendMessage(receivedMessage.channel, `naughty turtle`);
+    return tryMessageReact(receivedMessage, `:brainlet:`);
   }
 }
 
@@ -334,8 +350,8 @@ function rejectDNSRecord(args, receivedMessage) {
   if (isGod(message.author.id)) {
     // Do logic here
   } else {
-    tryChannelSendMessage(receivedMessage.channel, "naughty turtle");
-    return tryMessageReact(receivedMessage, ":brainlet:");
+    tryChannelSendMessage(receivedMessage.channel, `naughty turtle`);
+    return tryMessageReact(receivedMessage, `:brainlet:`);
   }
 }
 
